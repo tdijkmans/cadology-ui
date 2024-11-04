8a0b846ba485f07848b55512ee1f627f32d661a8
Thomas Dijkmans <tdijkmans@gmail.com>
2024-11-04 21:26:52 +0100

    refactor: convert AppComponent to standalone and remove AppModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [
+    CommonModule
+  ]
 })
 export class AppComponent {
   title = 'cadology-ui';
