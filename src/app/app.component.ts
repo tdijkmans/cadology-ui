2f74e35aaaabe6d4754a6166e5f984b7916ea011
Thomas Dijkmans <tdijkmans@gmail.com>
2024-11-04 21:43:42 +0100

    feat: add DataService for fetching items and configure app with HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DataService } from './services/data.service';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule]
 })
 export class AppComponent {
   title = 'cadology-ui';
+  items: Observable<Object> = new Observable();
+
+  constructor(
+    private d: DataService
+  ) {
+  }
+
+  ngOnInit() {
+    this.items = this.d.getItems();
+
+  }
 }
